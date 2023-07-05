c6add4ff8a7cf9b39923f444d7faaa0c0e2bcd06
4LV4R02010 <104158271+4LV4R02010@users.noreply.github.com>
2023-07-05 16:02:10 -0500

    Add files via upload

---
+++ Braille Web Project/App Page/magic.js
@@ -0,0 +1,9 @@
+document.getElementById("myButton").addEventListener("mouseenter", function() {
+    this.classList.add("button-link");
+  });
+  
+  document.getElementById("myButton").addEventListener("mouseleave", function() {
+    this.classList.remove("button-link");
+  });
+  
+
---
+++ Braille Web Project/Sobre Nosotros Page/script.js
@@ -0,0 +1,6 @@
+alert(`Bienvenido`);
+document.querySelector(`h1`).addEventListener(`hover`, function name(params) {
+    console.log(`You just achieved it`)
+})
+
+let 
